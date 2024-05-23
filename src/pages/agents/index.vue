6dfa28ea7ed2989c08133f443803a458f4f81100
chloe-vincent03 <chloe.vincent03@edu.univ-fcomte.fr>
2024-05-23 11:50:49 +0200

    essaye de faire agent

--- src/pages/agents/index.vue
+++ src/pages/agents/index.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { getAllAgents } from '@/backend'; 
+
+const agents = ref([])
+
+onMounted(async () => {
+  agents.value = await getAllAgents()
+})
 </script>
 
 <template>
   <div>
     <h1 class="text-xl">Tous les agents</h1>
     <ul>
-        
+      <li v-for="agent in agents" :key="agent.id">
+        {{ agent.nom }} - {{ agent.email }}
+      </li>
     </ul>
-    
   </div>
+</template>
