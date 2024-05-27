40027db7cf996277e36e7c659de7e46d1d6bcb47
chloe-vincent03 <chloe.vincent03@edu.univ-fcomte.fr>
2024-05-27 10:36:23 +0200

    debut de la page agents

--- src/pages/agents/index.vue
+++ src/pages/agents/index.vue
@@ -1,21 +1,22 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+/* import { ref, onMounted } from 'vue';
 import { getAllAgents } from '@/backend'; 
 
 const agents = ref([])
 
 onMounted(async () => {
   agents.value = await getAllAgents()
-})
+}) */
 </script>
 
 <template>
   <div>
     <h1 class="text-xl">Tous les agents</h1>
-    <ul>
+   <!--  <ul>
       <li v-for="agent in agents" :key="agent.id">
         {{ agent.nom }} - {{ agent.email }}
       </li>
-    </ul>
+    </ul> -->
   </div>
 </template>
